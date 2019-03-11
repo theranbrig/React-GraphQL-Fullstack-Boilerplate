f5bd17d528ff2cc5ed9fcf908fe575f1198942e1
Theran Brigowatz <theran.brigowatz@gmail.com>
2019-03-12 00:17:08 +0900

    updated backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,13 +5,13 @@ const db = require('./db');
 const server = createServer();
 
 server.start(
-	{
-		cors: {
-			credentials: true,
-			origin: process.env.FRONTEND_URL
-		}
-	},
-	deets => {
-		console.log(`Server is now running on port http:/localhost:${deets.port}`);
-	}
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => {
+    console.log(`Server is now running on port http:/localhost:${deets.port}`);
+  }
 );
